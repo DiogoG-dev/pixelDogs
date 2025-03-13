fef697887f5bcb81feeaa71d04ee7f103c65571f
DiogoGabriel <diogogabriel.tech@gmail.com>
2025-03-13 16:25:16 -0300

    Some corrections and adding sort-down

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ let currentUrl = 'https://dog.ceo/api/';
 
 async function getInfoBreeds() {
     try {
-        const response = await fetch(`${currentUrl}breed/hound/images/random`);
+        const response = await fetch(`${currentUrl}breeds/image/random`);
         const responseJson = await response.json();
 
-        document.getElementById('img').src = responseJson.message;
+        const urlImg = responseJson.message;
+        const base = "https://images.dog.ceo/breeds/";
+        const twoPath = urlImg.replace(base, "")
+        let breed = twoPath.split("/")[0];
+
+        breed = breed.toUpperCase()
+
+        document.getElementById('img').src = urlImg;
+        let nameDog = document.getElementById('name-dog')
+        nameDog.innerText = breed
     } catch (error) {
         console.log(error)
     }
