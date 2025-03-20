cff8ad2bde96cc41d5bef274dd163305b5c7b826
DiogoGabriel <diogogabriel.tech@gmail.com>
2025-03-20 16:55:56 -0300

    Adding function game - guess the dog

--- pages/guessMiniGame/guess.js
+++ pages/guessMiniGame/guess.js
@@ -15,43 +15,36 @@ async function getDataForGame(url) {
     const responseJson = await response.json();
     infoDogs = responseJson.message;
 
-    // Escolhe um cachorro aleatório
     const randomDogUrl = infoDogs[Math.floor(Math.random() * infoDogs.length)];
 
-    // Define a imagem no jogo
     const imgDog = document.getElementById('img-dog');
     imgDog.style.backgroundImage = `url(${randomDogUrl})`;
 
-    // Extrai a raça do nome da URL
     const regex = /breeds\/([^\/]+)\//;
-    const match = randomDogUrl.match(regex);
-    const correctBreed = match[1];
+    const correctBreed = randomDogUrl.match(regex)[1].replace('-', ' ').toUpperCase();
 
-    // Lista todas as raças
-    let alternatives = infoDogs.map(url => url.match(regex)[1]);
+    let alternatives = infoDogs.map(url => url.match(regex)[1].replace('-', ' ').toUpperCase());
 
-    // Embaralha as alternativas
     alternatives = alternatives.sort(() => Math.random() - 0.5);
 
-    // Define os botões com as alternativas
     const buttons = [
         document.getElementById('first-alternative'),
         document.getElementById('second-alternative'),
         document.getElementById('third-alternative'),
         document.getElementById('fourth-alternative')
-    ];
+    ]
 
     buttons.forEach((button, index) => {
-        button.innerText = alternatives[index]; // Define o texto do botão
-        button.onclick = () => checkAnswer(alternatives[index], correctBreed); // Verifica a resposta ao clicar
-    });
+        button.innerText = alternatives[index];
+        button.onclick = () => checkAnswer(alternatives[index], correctBreed)
+    })
 }
 
 // Função para verificar se a resposta está correta
 function checkAnswer(selected, correct) {
-    if (selected === correct) {
-        alert("✅ Correto!");
+    if(selected === correct) {
+        alert('Acertooooooooooou!')
     } else {
-        alert("❌ Errado! A resposta certa era " + correct);
+        alert('Errooooooooooooou!')
     }
 }
